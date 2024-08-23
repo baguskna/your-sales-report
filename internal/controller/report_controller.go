83575084ed66eb02dbce997646560ab37dd0a710
Bagus Kurnia <bagus.kurnia@sirclo.com>
2024-08-23 21:11:11 +0700

    feat: add go echart

--- internal/controller/report_controller.go
+++ internal/controller/report_controller.go
@@ -1,16 +1,20 @@
 package controller
 
 import (
+	"bytes"
+	"html/template"
 	"net/http"
 	"your-sales-report/internal/domain"
 	"your-sales-report/internal/service"
 
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
 	"github.com/labstack/echo/v4"
 )
 
 type ReportData struct {
-	TotalGMV                string
-	TotalOrderAndPercentage []domain.TotalOrderAndPercentage
+	TotalGMV     string
+	GMVChartHTML template.HTML
 }
 
 type ReportController struct {
@@ -34,9 +38,35 @@ func (h *ReportController) GetReports(c echo.Context) error {
 	}
 
 	data := ReportData{
-		TotalGMV:                valueGMV.Value,
-		TotalOrderAndPercentage: orders,
+		TotalGMV:     valueGMV.Value,
+		GMVChartHTML: generateTotalOrderChart(orders),
 	}
 
 	return c.Render(200, "index", data)
+}
+
+func generateTotalOrderChart(orders []domain.TotalOrderAndPercentage) template.HTML {
+	bar := charts.NewBar()
+
+	bar.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{Title: "Total Order"}),
+	)
+
+	xAxis := make([]string, len(orders))
+	yAxis := make([]opts.BarData, len(orders))
+
+	for i, order := range orders {
+		xAxis[i] = order.Marketplace
+		yAxis[i] = opts.BarData{Value: order.TotalOrder}
+	}
+
+	bar.SetXAxis(xAxis).
+		AddSeries("Total Order", yAxis)
+
+	var buf bytes.Buffer
+	if err := bar.Render(&buf); err != nil {
+		panic(err)
+	}
+
+	return template.HTML(buf.String())
 }
